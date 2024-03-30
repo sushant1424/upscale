89e7fe5ee37f4a6d4ccfd15a4bf82b8450b547ab
sushant1424 <sushantshrestha184@gmail.com>
2024-03-30 20:31:28 +0545

    remove div with class top and design display_event and info pages

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <title>Upscale Events</title>
 </head>
 <body>
-  <div class="top">
+  
     <?php
     require_once "header.php";
     ?>
@@ -21,10 +21,40 @@
           <img src="pictures/coverimage.jpg" alt="Event Cover Picture"/>
       </div>
     </div>
+  
+
+
+  <div class="display-event">
+    <h2>We provide services to manage many events.</h2>
+    <div class="gallery">
+      <img src="pictures/birthday.jpg" alt="birthday">
+      <img src="pictures/marriage.jpg" alt="marriage">
+      <img src="pictures/babyshower.jpg" alt="babyshower">
+      <img src="pictures/corportae.jpg" alt="corporate">
+    </div>
+    <button>Explore Events </button>
   </div>
 
   <div class="info">
-
+    <h2>How it works.</h2>
+    <div class="cards">
+      <div class="card">
+        <img src="pictures/userregister.png" alt="User Register Image"/>
+        <p>Create account and connect with us.</p>
+      </div>
+      <div class="card">
+        <img src="pictures/eventregister.jpg" alt="User Register Image"/>
+        <p>Search, create and register many events.</p>
+      </div>
+      <div class="card">
+        <img src="pictures/enjoy.jpg" alt="User Register Image"/>
+        <p>Enjoy and celebrate your beautiful events.</p>
+      </div>
+    </div>
+  
   </div>
+
+
+ 
 </body>
 </html>
