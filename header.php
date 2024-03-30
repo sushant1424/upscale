015399537d1f54940d9cfc09e3b3d0902d8381ee
sushant1424 <sushantshrestha184@gmail.com>
2024-03-30 17:46:11 +0545

    design nav bar and index page

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="header.css">
+  <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css"
+    rel="stylesheet"/>
+  <title>Header</title>
+</head>
+<body>
+  <header>
+  <img src="pictures/logo.png" alt="upscale-logo" >
+  <nav>
+    <a href=""><h4>About</h4></a>
+    <a href=""><h4>Services</h4></a>
+    <a href=""><h4>Events</h4></a>
+    <a href=""><h4>Contact</h4></a>
+    <button>Sign In</button>
+  </nav>
+  <i class="ri-menu-line"></i>
+
+  </header>
+</body>
+</html>
