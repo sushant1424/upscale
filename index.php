e37ad2f76ddf309f7113b512bed43b856c86bab3
sushant1424 <sushantshrestha184@gmail.com>
2024-03-30 17:53:53 +0545

    add a new div with class top

--- index.php
+++ index.php
@@ -7,22 +7,24 @@
   <title>Upscale Events</title>
 </head>
 <body>
-  <?php
-  require_once "header.php";
-  ?>
-  <div class="content">
-    <div class="left">
-      <h1>Elevate Your <span>Events</span> and <span>Memories</span> With Us.</h1>
-      <p>Looking for a great place to organize and manage your events? You are in the right place.</p>
-      <button>Get Started</button>
-    </div>
-    <div class="right">
-        <img src="pictures/coverimage.jpg" alt="Event Cover Picture"/>
+  <div class="top">
+    <?php
+    require_once "header.php";
+    ?>
+    <div class="content">
+      <div class="left">
+        <h1>Elevate Your <span>Events</span> and <span>Memories</span> With Us.</h1>
+        <p>Looking for a great place to organize and manage your events? You are in the right place.</p>
+        <button>Get Started</button>
+      </div>
+      <div class="right">
+          <img src="pictures/coverimage.jpg" alt="Event Cover Picture"/>
+      </div>
     </div>
   </div>
 
   <div class="info">
-    
+
   </div>
 </body>
 </html>
